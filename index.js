a7fe4648d858d752bb76948b05632ef9f3e3d671
Max Schmeling <max@maxschmeling.me>
2019-07-26 17:42:39 -0400

    Remove xcodeproj.

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = function loginKit() {
+  console.log("It works!");
+}
--- ios/SnapchatLoginManagerBridge.m
+++ ios/SnapchatLoginManagerBridge.m
@@ -1,11 +1,3 @@
-//
-//  SnapchatLoginManagerBridge.m
-//  ReactNativeLoginKit
-//
-//  Created by Max Schmeling on 7/26/19.
-//  Copyright © 2019 Max Schmeling. All rights reserved.
-//
-
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(SnapLoginManager, NSObject)
