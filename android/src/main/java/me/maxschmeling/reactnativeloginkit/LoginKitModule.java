3815f7c772dbab8047300e78ae1d2a8a0495136d
Max Schmeling <max@maxschmeling.me>
2019-08-14 14:02:22 -0500

    Add login method and dependencies

--- android/src/main/java/me/maxschmeling/reactnativeloginkit/LoginKitModule.java
+++ android/src/main/java/me/maxschmeling/reactnativeloginkit/LoginKitModule.java
@@ -1,9 +1,18 @@
 package me.maxschmeling.reactnativeloginkit;
 
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import com.snapchat.kit.sdk.SnapLogin;
+import com.snapchat.kit.sdk.core.controller.LoginStateController;
+import com.snapchat.kit.sdk.login.models.MeData;
+import com.snapchat.kit.sdk.login.models.UserDataResponse;
+import com.snapchat.kit.sdk.login.networking.FetchUserDataCallback;
 
 public class LoginKitModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +29,61 @@ public class LoginKitModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void login(Promise callback) {
+        final LoginStateController.OnLoginStateChangedListener loginStateChangedListener =
+            new LoginStateController.OnLoginStateChangedListener() {
+            @Override
+            public void onLoginSucceeded() {
+                SnapLogin.getLoginStateController(getReactApplicationContext()).removeOnLoginStateChangedListener(this);
+
+                String query = "{me{bitmoji{avatar},displayName,externalId}}";
+                SnapLogin.fetchUserData(getReactApplicationContext(), query, null, new FetchUserDataCallback() {
+                    @Override
+                    public void onSuccess(@Nullable UserDataResponse userDataResponse) {
+                        if (userDataResponse == null || userDataResponse.getData() == null) {
+                            promise.reject("No userDataResponse data.");
+                            return;
+                        }
+
+                        MeData meData = userDataResponse.getData().getMe();
+                        if (meData == null) {
+                            promise.reject("No meData.");
+                            return;
+                        }
+
+
+                        WritableMap result = Arguments.createMap();
+
+                        result.putString("displayName", meData.getDisplayName());
+                        result.putString("bitmojiAvatarUrl", meData.getBitmojiData().getAvatar());
+                        result.putString("externalId", meData.getExternalId());
+                        result.putString("accessToken", SnapLogin.getAuthTokenManager(getReactApplicationContext()).getAccessToken());
+
+                        promise.resolve(result);
+                    }
+
+                    @Override
+                    public void onFailure(boolean isNetworkError, int statusCode) {
+                        promise.reject("Unknown Error");
+                    }
+                });
+            }
+
+            @Override
+            public void onLoginFailed() {
+                SnapLogin.getLoginStateController(getReactApplicationContext()).removeOnLoginStateChangedListener(this);
+                promise.reject("Unknown Error");
+            }
+
+            @Override
+            public void onLogout() {
+                // Not applicable in this context
+            }
+        };
+
+        SnapLogin.getLoginStateController(getReactApplicationContext())
+                .addOnLoginStateChangedListener(loginStateChangedListener);
+
+        SnapLogin.getAuthTokenManager(getReactApplicationContext()).startTokenGrant();
     }
 }
