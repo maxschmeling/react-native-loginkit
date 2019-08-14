51be5c0c795acf8706d191777c38dec6db74a52f
Max Schmeling <max@maxschmeling.me>
2019-08-14 13:20:37 -0500

    Update names left-over from copy & paste

--- android/src/main/java/me/maxschmeling/reactnativeloginkit/LoginKitModule.java
+++ android/src/main/java/me/maxschmeling/reactnativeloginkit/LoginKitModule.java
@@ -5,18 +5,18 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class CreativeKitModule extends ReactContextBaseJavaModule {
+public class LoginKitModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
 
-    public CreativeKitModule(ReactApplicationContext reactContext) {
+    public LoginKitModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
     }
 
     @Override
     public String getName() {
-        return "CreativeKit";
+        return "LoginKit";
     }
 
     @ReactMethod
--- android/src/main/java/me/maxschmeling/reactnativeloginkit/LoginKitPackage.java
+++ android/src/main/java/me/maxschmeling/reactnativeloginkit/LoginKitPackage.java
@@ -10,10 +10,10 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
-public class CreativeKitPackage implements ReactPackage {
+public class LoginKitPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new CreativeKitModule(reactContext));
+        return Arrays.<NativeModule>asList(new LoginKitModule(reactContext));
     }
 
     @Override
