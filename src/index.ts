bd20872b2bb916657f98c76b9b2b7ef3189b53d8
Max Schmeling <max@maxschmeling.me>
2021-01-28 15:21:48 -0600

    Add prettier and upgrade SnapSDK

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ const { NativeModules } = require("react-native");
 
 const { SnapchatLoginManager } = NativeModules;
 
-export function login():Promise<void> {
+export function login(): Promise<void> {
   return SnapchatLoginManager.login();
 }
